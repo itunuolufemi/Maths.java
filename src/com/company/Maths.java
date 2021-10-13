f64d79db876d48bcc710417d48b728be7ba2133a
itunuolufemi <olufemiitunu98@gmail.com>
2021-10-13 08:11:29 -0500

    Initial commit

---
+++ src/com/company/Maths.java
@@ -0,0 +1,96 @@
+package com.company.Maths;
+
+public class Maths {
+
+    public static void main(String[]args) {
+            String Gender = "Hello World";
+            System.out.println("Hello World");
+
+            //ARITHMETIC EXPRESSIONS
+           // int result = 10%3;
+        //System.out.println(result);
+
+        float result = 10/3F;
+        System.out.println(result);
+
+        //int x = 1;
+       // ++x;
+        //System.out.println(x);
+
+        //Postfix and Pre-fix
+        //int x = 1;
+        //int y = x++;
+       // System.out.println(x);
+        //System.out.println(y);
+
+        //int x = 1;
+        //x = x+2;
+        // System.out.println(x);
+
+        //int x = (10+3)-2;
+        //System.out.println(x);
+
+        //IMPLICIT CASTING/AUTOMATIC CASTING/AUTOMATIC CONVERSION
+        //byte > short > int > long > float > double
+
+        //short x = 1;
+        // int y = x + 2;
+        //System.out.println(y);
+
+        //double x = 1.1;
+       // double y = x + 2;
+        //System.out.println(y);
+
+        //Converting from int to double
+        //int x = 1;
+        //double y = x + 2;
+        //System.out.println(y);
+
+        //Converting from byte to integer
+        //byte x = 1;
+        //int y = x + 2;
+        //System.out.println(y);
+
+        //Converting from Integer to Long
+        //int x = 1;
+        //long y = x + 2;
+        //System.out.println(y);
+
+        //Converting from Integer to Float
+        int x = 5;
+        float y = x + 10;
+        System.out.println(y);
+
+        //Another Implicit Data Type Casting Example
+        int a = 20;
+        long b = a;
+        double c = b;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+
+        //EXPLICIT CASTING
+        //Double > Float > Long > Int > Char > Short > Byte
+        //Examples of explicit casting
+        double d = 57.17;
+        int i = (int)d;
+        System.out.println(i);
+
+        double f = 60.30;
+        short e = (short)f;
+        System.out.println(e);
+
+        char z = '5';
+        byte n = (byte)z;
+        System.out.println(n);
+
+
+
+
+
+
+        }
+    }
+
